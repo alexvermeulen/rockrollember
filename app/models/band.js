790f131e2a31336c7ccc051bf94425db54c17841
Alex Vermeulen <avermeulen@freshbooks.com>
2016-04-18 08:58:22 -0400

    Chapter 8: Controllers

--- app/models/band.js
+++ app/models/band.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export default Ember.Object.extend({
   name: '',
 
+  slug: Ember.computed('name', function() {
+    return this.get('name').dasherize();
+  }),
+
   setupSongs: Ember.on('init', function() {
     if (!this.get('songs')) {
       this.set('songs', []);
     }
-  }),
-
-  slug: Ember.computed('name', function() {
-    return this.get('name').dasherize();
   }),
 });
